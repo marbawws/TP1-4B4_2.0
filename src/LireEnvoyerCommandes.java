fd7e26872e5ff6c5681874f56cc02aaeb77095e8
Marwane Rezgui <clibeau2010@hotmail.com>
2020-02-11 15:53:21 -0500

    On pont maintenant lire le fichier et extraire les informations par lignes dans un tableau

--- src/LireEnvoyerCommandes.java
+++ src/LireEnvoyerCommandes.java
@@ -1,4 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import outilsjava.OutilsFichier;
 
 public class LireEnvoyerCommandes {
 
+	public static void main(String[] args) {
+		String[] tableauInformation = lireFichierEtMettreDansTableau();
+	}
+
+	private static String[] lireFichierEtMettreDansTableau() {
+		
+		int nblignes = 0;
+		String[] tableauInformation = null;
+		BufferedReader informations = OutilsFichier.ouvrirFicTexteLecture("fichierEntree.txt");
+		
+		if (informations == null) {
+			//le fichier n'existe pas 
+		} else {
+			
+			try {
+				while (informations.readLine() != null) nblignes++; // trouver le nombre de lignes
+				informations = OutilsFichier.ouvrirFicTexteLecture("fichierEntree.txt"); //reload le buffereader pour relire les lignes
+				tableauInformation = new String[nblignes];
+				
+				for (int i = 0; i < nblignes; i++) {
+					String ligneTexte = informations.readLine();
+					tableauInformation[i] = ligneTexte; 
+				}//extraire toutes les lignes dans un tableau
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Erreur pour lire la ligne");
+			}
+		}
+		return tableauInformation;
+	}
+	
+	private boolean verifier(String[] tableauInformation) {
+		if (tableauInformation[0] == "Clients:" && tableauInformation[0] == "Clients:" ) {
+		}
+
+		return true;
+	}
 }
