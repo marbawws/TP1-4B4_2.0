bb831c66b1fc0286e0570070b93a76ae29c95549
Marwane Rezgui <clibeau2010@hotmail.com>
2020-02-23 10:27:39 -0500

    .

--- src/LireEnvoyerCommandes.java
+++ src/LireEnvoyerCommandes.java
@@ -14,7 +14,7 @@ public class LireEnvoyerCommandes {
 	private static final String LIGNE_CLIENTS_ENTREE = "Clients :";
 	private static final String LIGNE_PLATS_ENTREE = "Plats :";
 	private static final String LIGNE_COMMANDES_ENTREE = "Commandes :";
-	
+
 	public static void main(String[] args) throws IOException {
 
 		String[] tableauInformation = lireFichierEtMettreDansTableau();
@@ -27,119 +27,119 @@ public class LireEnvoyerCommandes {
 			} else {
 				System.out.println("Le fichier ne respecte pas le format demandé !\n\nArrêt du programme.");
 			}
-			
+
 		} else {
 			System.out.println("Le fichier ne respecte pas le format demandé !\n\nArrêt du programme.");
 		}
-		
+
 	}
 
 	private static boolean verifierIntegriteClients() {
 		boolean valide = true;
-		
+
 		for (int i = 0; i < listeCommandes.size(); i++) {
-			if (chercherIndexClient(listeCommandes.get(i).getNomClient()) < 0){
+			if (chercherIndexClient(listeCommandes.get(i).getNomClient()) < 0) {
 				valide = false;
 			}
 		}
-		
+
 		return valide;
 	}
-	
+
 	private static boolean verifierIntegritePlats() {
 		boolean valide = true;
-		
+
 		for (int i = 0; i < listeCommandes.size(); i++) {
-			if (chercherIndexPlat(listeCommandes.get(i).getNomPlat()) < 0){
+			if (chercherIndexPlat(listeCommandes.get(i).getNomPlat()) < 0) {
 				valide = false;
 			}
 		}
-		
+
 		return valide;
 	}
-	
+
 	private static void creerFacture() throws IOException {
-		
+
 		// Vérifier si le client a des commandes à son nom.
 		BufferedWriter informationOutput = OutilsFichier.ouvrirFicTexteEcriture("FichierSortie.txt");
 		informationOutput.write("Bienvenue chez Barette!\nFactures :\n");
-		
+
 		for (int i = 0; i < listeClients.size(); i++) {
 			ArrayList<Commande> commandesClientCourant = chercherCommandesClient(listeClients.get(i).getNom());
 			double prix = 0;
-			
+
 			if (commandesClientCourant.isEmpty()) {
 
-				
 				informationOutput.write(listeClients.get(i).getNom() + " " + String.format("%.2f", prix) + "$");
 				informationOutput.newLine();
-				
+
 			} else {
-				
+
 				for (int j = 0; j < commandesClientCourant.size(); j++) {
-					//Additionner le prix de chaque commande faite par le client.
+					// Additionner le prix de chaque commande faite par le client.
 					prix += commandesClientCourant.get(j).getPrixPlat() * commandesClientCourant.get(j).getNbPlats();
-					
+
 				}
-	
+
 				informationOutput.write(listeClients.get(i).getNom() + " " + String.format("%.2f", prix) + "$");
 				informationOutput.newLine();
 			}
-			
+
 		}
 		informationOutput.close();
 	}
-	
+
 	/*
-	Retourne une liste contenant les commandes du client indiqué en paramètre. Retourne une liste vide
-	si le client n'a rien commandé.
-	*/
+	 * Retourne une liste contenant les commandes du client indiqué en paramètre.
+	 * Retourne une liste vide si le client n'a rien commandé.
+	 */
 	private static ArrayList<Commande> chercherCommandesClient(String nomClient) {
 		ArrayList<Commande> commandesClient = new ArrayList<Commande>();
-		
+
 		for (int i = 0; i < listeClients.size(); i++) {
 			if (nomClient.equals(listeCommandes.get(i).getNomClient())) {
 				commandesClient.add(listeCommandes.get(i));
 			}
 		}
-		
+
 		return commandesClient;
 	}
-	
-	//Retourne l'index du plat indiqué en paramètre ou -1 si le plat n'existe pas.
+
+	// Retourne l'index du plat indiqué en paramètre ou -1 si le plat n'existe pas.
 	private static int chercherIndexPlat(String nomPlat) {
 		int index = -1;
-		
+
 		for (int i = 0; i < listePlats.size(); i++) {
 			if (nomPlat.equals(listePlats.get(i).getNomPlat())) {
 				index = i;
 				break;
 			}
 		}
-		
+
 		return index;
 	}
-	
+
 	private static int chercherIndexClient(String nomClient) {
 		int index = -1;
-		
+
 		for (int i = 0; i < listeClients.size(); i++) {
 			if (nomClient.equals(listeClients.get(i).getNom())) {
 				index = i;
 				break;
 			}
 		}
-		
+
 		return index;
 	}
-	
-	//Cette méthode popule les listes contenant les clients, les plats et les commandes.
+
+	// Cette méthode popule les listes contenant les clients, les plats et les
+	// commandes.
 	private static void creerListes(String[] tableauInformation) {
-		String mode = "Clients"; //On sait que la première catégorie est clients.
-		
-		//On commence après «Clients :» et on ignore la dernière ligne du fichier.
-		for (int i = 1; i < tableauInformation.length - 1; i++) { 
-			if(tableauInformation[i].equals(LIGNE_PLATS_ENTREE)) {
+		String mode = "Clients"; // On sait que la première catégorie est clients.
+
+		// On commence après «Clients :» et on ignore la dernière ligne du fichier.
+		for (int i = 1; i < tableauInformation.length - 1; i++) {
+			if (tableauInformation[i].equals(LIGNE_PLATS_ENTREE)) {
 				mode = "Plats";
 			} else if (tableauInformation[i].equals(LIGNE_COMMANDES_ENTREE)) {
 				mode = "Commandes";
@@ -151,29 +151,29 @@ public class LireEnvoyerCommandes {
 
 				case "Plats":
 					listePlats.add(new Plat(tableauInformation[i].trim().split(" ")[0],
-						Double.parseDouble(tableauInformation[i].trim().split(" ")[1])));
+							Double.parseDouble(tableauInformation[i].trim().split(" ")[1])));
 					break;
 				case "Commandes":
 					listeCommandes.add(new Commande(tableauInformation[i].trim().split(" ")[0],
 							tableauInformation[i].trim().split(" ")[1],
 							Integer.parseInt(tableauInformation[i].trim().split(" ")[2])));
-					
+
 					int indexPlat = chercherIndexPlat(listeCommandes.get(listeCommandes.size() - 1).getNomPlat());
-					
+
 					if (indexPlat < 0) {
 						System.out.println("Erreur");
 					} else {
 						listeCommandes.get(listeCommandes.size() - 1).setPrixPlat(listePlats.get(indexPlat).getPrix());
 					}
 					break;
-					
+
 				default:
 					break;
 				}
 			}
 		}
 	}
-	
+
 	private static String[] lireFichierEtMettreDansTableau() {
 
 		int nblignes = 0;
@@ -185,9 +185,10 @@ public class LireEnvoyerCommandes {
 		} else {
 
 			try {
-				while (informations.readLine() != null) nblignes++; // trouver le nombre de lignes
+				while (informations.readLine() != null)
+					nblignes++; // trouver le nombre de lignes
 				informations = OutilsFichier.ouvrirFicTexteLecture("fichierEntree.txt"); // reload le buffereader pour
-																						// relire les lignes
+																							// relire les lignes
 				tableauInformation = new String[nblignes];
 
 				for (int i = 0; i < nblignes; i++) {
@@ -196,60 +197,72 @@ public class LireEnvoyerCommandes {
 				} // extraire toutes les lignes dans un tableau
 
 			} catch (IOException e) {
-				
+
 				System.out.println("Une erreur est survenue lors de la lecture du fichier.");
 			}
 		}
 		return tableauInformation;
 	}
-	
-	/*Cette méthode vérifie si chacune des lignes du tableau envoyer en paramètre est conforme au format*/
-	
-	//TODO Certains cas d'erreurs ne sont pas vérifiés pour la liste des commandes.
-	//Je suggère également d'avoir un seul énoncé return à la fin de la méthode.
+
+	/*
+	 * Cette méthode vérifie si chacune des lignes du tableau envoyer en paramètre
+	 * est conforme au format
+	 */
+
 	private static boolean verifierFormatFic(String[] tableauInformation) {
 		int lignePlats = 0;
 		int ligneCommandes = 0;
+		boolean verifier = true;
 		if (tableauInformation[0].equals(LIGNE_CLIENTS_ENTREE)) {
-
-			for (int i = 0; i < tableauInformation.length && lignePlats == 0; i++) {
-				if (tableauInformation[i].equals(LIGNE_PLATS_ENTREE)) {
-					lignePlats = i;
-				} // la ligne ou le mot Plats: se situe
-			}
-
-			for (int i = 1; i < lignePlats; i++) {
-				if (tableauInformation[i].contains(" ")) {
-					return false;
-				} // si un nom contient un espace
-			}
-
-			for (int i = 0; i < tableauInformation.length && ligneCommandes == 0; i++) {
-				if (tableauInformation[i].equals(LIGNE_COMMANDES_ENTREE)) {
-					ligneCommandes = i;
-				} // la ligne ou le mot Commandes: se situe
-			}
-
-			for (int i = lignePlats + 1; i < ligneCommandes; i++) {
-				if (!tableauInformation[i].matches("^.[a-zA-Z_]+ [0-9.]+$")) {
-					return false;
-				} // si un plat contient un espace avant le nombre
-			}
-			if (!tableauInformation[tableauInformation.length - 1].equals("Fin")) {
-				return false;
-			}
 			
-			for (int i = ligneCommandes + 1; i < tableauInformation.length - 1 ; i++) {
-				if (!tableauInformation[i].matches("^.[a-zA-Z\u00C0-\u00FF]+ [a-zA-Z_]+ [0-9]+$")) {
-					return false;
-				} // si nom espace plat espace nombre
+			if (verifier) {
+
+				for (int i = 0; i < tableauInformation.length && lignePlats == 0; i++) {
+					if (tableauInformation[i].equals(LIGNE_PLATS_ENTREE)) {
+						lignePlats = i;
+					} // la ligne ou le mot Plats: se situe
+				}
+
+				for (int i = 1; i < lignePlats; i++) {
+					if (tableauInformation[i].contains(" ")) {
+						verifier = false;
+					} // si un nom contient un espace
+				}
+				
+				if (verifier) {
+
+					for (int i = 0; i < tableauInformation.length && ligneCommandes == 0; i++) {
+						if (tableauInformation[i].equals(LIGNE_COMMANDES_ENTREE)) {
+							ligneCommandes = i;
+						} // la ligne ou le mot Commandes: se situe
+					}
+
+					for (int i = lignePlats + 1; i < ligneCommandes; i++) {
+						if (!tableauInformation[i].matches("^.[a-zA-Z_]+ [0-9.]+$")) {
+							verifier = false;
+						} // si un plat contient un espace avant le nombre
+					}
+					if (verifier) {
+						
+						if (!tableauInformation[tableauInformation.length - 1].equals("Fin")) {
+							verifier =  false;
+						}
+						if (verifier) {
+							for (int i = ligneCommandes + 1; i < tableauInformation.length - 1; i++) {
+								if (!tableauInformation[i].matches("^.[a-zA-Z\u00C0-\u00FF]+ [a-zA-Z_]+ [0-9]+$")) {
+									return false;
+								} // si nom espace plat espace nombre
+							}
+						}
+					}
+				}
 			}
 
 		} else {
 
-			return false;
+			verifier = false;
 		}
 
-		return true;
+		return verifier;
 	}
 }
