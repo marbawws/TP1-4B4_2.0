cc1e233a5e28d4e21562a53dce38a76ad29239a5
Marwane Rezgui <clibeau2010@hotmail.com>
2020-04-01 17:15:05 -0400

    infrastructure de test en boite noire est terminee

--- src/test/LireEnvoyerCommandesTest.java
+++ src/test/LireEnvoyerCommandesTest.java
@@ -2,46 +2,63 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 
 import org.junit.Test;
 
 import main.LireEnvoyerCommandes;
+import outilsjava.OutilsFichier;
 
 public class LireEnvoyerCommandesTest {
 
 	@Test
-	public void testfacture() {
-		String[] tableauInformation = {"Clients :",
-			"Roger",
-			"Céline",
-			"Steeve",
-			"Plats :",
-			"Poutine 10.5",
-			"Frites 2.5",
-			"Repas_Poulet 15.75",
-			"Commandes :",
-			"Roger Poutine 1",
-			"Céline Frites 2",
-			"Céline Repas_Poulet 1",
-			"Fin"};
-		if (LireEnvoyerCommandes.verifierFormatFic(tableauInformation)) {
-			
-			LireEnvoyerCommandes.creerListes(tableauInformation);
-			
-			if (LireEnvoyerCommandes.verifierIntegriteClients() && LireEnvoyerCommandes.verifierIntegritePlats()) {
-				try {
-					LireEnvoyerCommandes.creerFacture();
-				} catch (IOException e) {
-					//
-				}
-			} else {
-				System.out.println("Le fichier ne respecte pas le format demandé !\nArrêt du programme.");
-			}
-			
-		} else {
-			System.out.println("Le fichier ne respecte pas le format demandé !\nArrêt du programme.");
-		}
-	}
+	public void testfacture() throws IOException {
+		// test en blackbox
+		String[] input = { "Clients :", "Roger", "Céline", "Steeve", "Plats :", "Poutine 10.5",
+				"Frites 2.5", "Repas_Poulet 15.75", "Commandes :", "Roger Poutine 1", "Céline Frites 2",
+				"Céline Repas_Poulet 1", "Fin" }; // changer le input a guise
+		String[] factureAttendu = { "Bienvenue chez Barette!", "Factures :", "", "Roger 10.50$", "Céline 20.75$",
+				"Steeve 0.00$" } // le output que l'on veut avoir
+		;
+		changerInput(input);//changer le fichier entree pour faire nos tests
+		LireEnvoyerCommandes.main(null);//appeler main pour faire le traitement
+		String[] factureRecue = lireSortieFacture();//recuperer le resultat
+		assertArrayEquals(factureAttendu, factureRecue);// faire le test avec le output voulu
 
+		}
+	
+	private void changerInput(String[] input) throws IOException {
+		BufferedWriter informationInput = OutilsFichier.ouvrirFicTexteEcriture("FichierEntree.txt");
+
+		for (int j = 0; j < input.length; j++) {
+			informationInput.write(input[j]);
+			informationInput.newLine();
+		}
+		
+		informationInput.close();
+	}
+	private String[] lireSortieFacture() throws IOException {
+		int nblignes = 0;
+		String[] factureRecue = null;
+		BufferedReader informationsOutput = OutilsFichier.ouvrirFicTexteLecture("fichierSortie.txt");
+
+		if (informationsOutput == null) {
+			// le fichier n'existe pas, le message d'erreur est déjà envoyer 
+		} else {
+			while (informationsOutput.readLine() != null)
+				nblignes++; // trouver le nombre de lignes
+			informationsOutput = OutilsFichier.ouvrirFicTexteLecture("fichierSortie.txt"); // reload le buffereader pour
+																							// relire les lignes
+			factureRecue = new String[nblignes];
+
+			for (int i = 0; i < nblignes; i++) {
+				String ligneTexte = informationsOutput.readLine();
+				factureRecue[i] = ligneTexte;
+			} // extraire toutes les lignes dans un tableau
+
+		}
+		return factureRecue;
+	}
 }
